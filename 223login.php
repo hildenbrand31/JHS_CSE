b4e9ae9181e77291d6a3889d6edfb80da64d58dd
Fred Routson <froutson@gmail.com>
2014-04-19 11:20:51 -0400

    initial project version 0.1

---
+++ 223login.php
@@ -0,0 +1,6 @@
+<?php
+$db_host = 'localhost';
+$db_database ='routson';
+$db_username = 'routson';
+$db_password = 'therE4fork&';
+?>
---
+++ login.php
@@ -0,0 +1,6 @@
+<?php
+$db_host = 'localhost';
+$db_database ='routson';
+$db_username = 'username';
+$db_password = 'password';
+?>
